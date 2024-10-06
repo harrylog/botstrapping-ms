e4c5debe6568794a1f7627cea40acdbe9d1b656b
harry log <harry8log@gmail.com>
2024-10-06 13:00:08 +0300

    update

--- D. terraform/resource-group.tf
+++ D. terraform/resource-group.tf
@@ -2,6 +2,6 @@
 # Creates a resource group for FlixTube in your Azure account.
 #
 resource "azurerm_resource_group" "flixtube" {
-  name     = "flixtube"
-  location = "eastus"
+  name     = var.app_name
+  location = var.location
 }
